18ff20193a39f9ae51f3fba9d9d1b6ae8eb5abb4
Han028 <han028@yu.ac.kr>
2025-06-03 08:48:15 +0900

    Modify assignment04User.cpp logic

--- Assignment04_admin.cpp
+++ Assignment04_admin.cpp
@@ -7,7 +7,7 @@
 #define PATH "accounts.txt"
 using namespace std;
 
-vector<string> split(string str)
+vector<string> split(string str) // 문자열 자르기
 {
 	vector<string> temp;
 	int startIndex = 0;
@@ -25,10 +25,10 @@ vector<string> split(string str)
 	return temp;
 }
 
-bool fileRead(map<string, vector<string>>& accounts) {
+bool fileRead(map<string, vector<string>>& accounts) { // 파일 읽기
 	ifstream fin(PATH);
-	if (!fin) {
-		cout << "파일이 존재하지 않습니다." << endl;
+	if (!fin) { // 파일 읽기 실패
+		cout << "[경고] 파일이 존재하지 않습니다." << endl;
 		ofstream fout(PATH);
 		fout.close();
 		return 0;
@@ -43,10 +43,10 @@ bool fileRead(map<string, vector<string>>& accounts) {
 	return 1;
 }
 
-bool filewrite(map<string, vector<string>> accounts) {
+bool fileWrite(map<string, vector<string>> accounts) { // 파일 쓰기
 	ofstream fout(PATH);
-	if (!fout) { // 열기 실패 검사
-		cout << "파일열기 실패" << endl;
+	if (!fout) { // 파일 열기 실패
+		cout << "[경고] 파일이 존재하지 않습니다." << endl;
 		return 0;
 	}
 
@@ -55,6 +55,7 @@ bool filewrite(map<string, vector<string>> accounts) {
 		fout << item.first << " " << item.second[0] << " " << item.second[1] << " " << item.second[2] << endl;
 	}
 	fout.close();
+	return 1;
 }
 
 void showAccount(map<string, vector<string>>& accounts){
@@ -71,22 +72,24 @@ int main(void) {
 
 	int select = 0; // 메뉴 선택 변수
 	while (select != 4) {
-		cout << "1. 계좌 등록\t2. 계좌 정보 수정\t3. 현황보기\t4. 종료 : ";
+		cout << "1. 계좌 등록\t2. 계좌정보수정\t3. 현황보기\t4. 종료 : ";
 		cin >> select;
 		switch(select) {
 			case 1: { // 계좌 등록
 				string accountNumber, owner, balance, password;
 				cout << "계좌번호 : "; cin >> accountNumber;
 				if (accounts.find(accountNumber) != accounts.end()) { // 이미 등록된 계좌인지 확인
-					cout << "이미 등록된 계좌입니다." << endl;
+					cout << "[실패] 이미 등록된 계좌입니다." << endl;
 					continue;
 				}
 				cout << "예금주 : "; cin >> owner;
 				cout << "잔액 : "; cin >> balance;
 				cout << "비밀번호 : "; cin >> password;
 				accounts[accountNumber] = { owner, balance, password };
-				cout << "등록되었습니다." << endl;
-				filewrite(accounts); // 파일에 저장
+				if (fileWrite(accounts)) {
+					cout << "[성공] 계좌가 등록되었습니다." << endl;
+				}
+				else cout << "[실패] 문제가 발생하였습니다." << endl;
 				break;
 			}
 			case 2: { // 계좌 정보 수정
@@ -94,15 +97,17 @@ int main(void) {
 				cout << "계좌번호 : "; cin >> accountNumber;
 				it = accounts.find(accountNumber);
 				if (it == accounts.end()) { // 존재하지 않는 계좌인지 확인
-					cout << "존재하지 않는 계좌입니다." << endl;
+					cout << "[실패] 존재하지 않는 계좌입니다." << endl;
 					continue;
 				}
 				cout << "예금주 : "; cin >> owner;
 				cout << "잔액 : "; cin >> balance;
 				cout << "비밀번호 : "; cin >> password;
 				it->second = { owner, balance, password }; // 정보 수정
-				cout << "변경되었습니다." << endl;
-				filewrite(accounts); // 파일에 저장
+				if (fileWrite(accounts)) {
+					cout << "[성공] 계좌 정보가 변경되었습니다." << endl;
+				}
+				else cout << "[실패] 문제가 발생하였습니다." << endl;
 				break;
 			}
 			case 3: { // 현황 보기
--- Assignment04_user.cpp
+++ Assignment04_user.cpp
@@ -7,46 +7,46 @@
 #define PATH "accounts.txt"
 using namespace std;
 
-vector<string> split(string str, string delimiter)
+vector<string> split(string str) // 문자열 자르기
 {
-	vector<string> v;
-	int sidx = 0;
+	vector<string> temp;
+	int startIndex = 0;
 	while (true)
 	{
-		int fidx = str.find(delimiter, sidx);
-		if (fidx == -1)
+		int findIndex = str.find(" ", startIndex);
+		if (findIndex == -1)
 		{
-			v.push_back(str.substr(sidx));
+			temp.push_back(str.substr(startIndex));
 			break;
 		}
-		v.push_back(str.substr(sidx, fidx - sidx));
-		sidx = fidx + delimiter.length();
+		temp.push_back(str.substr(startIndex, findIndex - startIndex));
+		startIndex = findIndex + 1;
 	}
-	return v;
+	return temp;
 }
 
-bool fileRead(string path, map<string, vector<string>>& accounts) {
-	ifstream fin(path);
-	if (!fin) {
-		cout << "파일이 존재하지 않습니다." << endl;
-		ofstream fout(path);
+bool fileRead(map<string, vector<string>>& accounts) { // 파일 읽기
+	ifstream fin(PATH);
+	if (!fin) { // 파일 읽기 실패
+		cout << "[경고] 파일이 존재하지 않습니다." << endl;
+		ofstream fout(PATH);
 		fout.close();
 		return 0;
 	}
 
 	string buf;
 	while (getline(fin, buf)) {
-		vector<string> line = split(buf, " ");
+		vector<string> line = split(buf);
 		accounts[line[0]] = { line[1], line[2], line[3] }; // 계좌번호 = 예금주, 잔액, 비밀번호
 	}
 	fin.close();
 	return 1;
 }
 
-bool filewrite(string path, map<string, vector<string>> accounts) {
-	ofstream fout(path);
-	if (!fout) { // 열기 실패 검사
-		cout << "파일열기 실패" << endl;
+bool fileWrite(map<string, vector<string>> accounts) { // 파일 쓰기
+	ofstream fout(PATH);
+	if (!fout) { // 파일 열기 실패
+		cout << "[경고] 파일이 존재하지 않습니다." << endl;
 		return 0;
 	}
 
@@ -55,81 +55,104 @@ bool filewrite(string path, map<string, vector<string>> accounts) {
 		fout << item.first << " " << item.second[0] << " " << item.second[1] << " " << item.second[2] << endl;
 	}
 	fout.close();
+	return 1;
 }
 
 int main(void) {
 	map<string, vector<string>> accounts; // 키: 계좌번호, 값 : [예금주, 잔액, 비밀번호]
 	map<string, vector<string>>::iterator it; // 자료 조회를 위한 포인터
-	fileread(PATH, accounts); // 계좌 정보 초기화 (불러오기)
+	fileRead(accounts); // 계좌 정보 초기화 (불러오기)
 
-	int init = 0;
 	string currentUser;
-	while (init != -1) {
-		cin >> init;
+	while (currentUser != "-1") {
 		cout << "계좌번호를 입력해주세요. (종료 : -1) >> ";
-		if (accounts.find(currentUser) != accounts.end()) { // 계좌번호가 accounts에 있을 경우
-			cout << "[성공] " << accounts[currentUser][0] << "님 환영합니다 ! " << endl;
+		cin >> currentUser;
+		if (currentUser == "-1") {
+			cout << "[종료] 프로그램을 종료합니다." << endl;
+		}
+		else if (accounts.find(currentUser) != accounts.end()) { // 계좌 번호가 accounts에 있을 경우
+			it = accounts.find(currentUser);
+			string accountNumber = it->first;
+			string owner = it->second[0]; // 값
+			string& balance = it->second[1];
+			string password = it->second[2];
+
+			cout << "[성공] " << owner << "님 환영합니다 ! " << endl;
+			
 			// 로그인 성공 : 메뉴 구현 구간
 			int select = 0; // 메뉴 선택 변수
-			while (select != 4) {
+			while (select != 5) {
 				cout << "1. 입금 \t 2. 출금 \t 3. 송금 \t 4. 조회 \t 5. 종료 >> "; cin >> select;
 				switch (select) {
-				case 1: { // 입금하기
-					int deposit;
-					cout << "입금액을 입력해주세요. >> "; cin >> deposit;
-					it->second[2] = to_string(stoi(it->second[2]) + deposit); // map의 value는 string으로 저장되어 있어서 변환 필요!
-					filewrite(PATH, accounts); // 파일에 저장
-					cout << "[성공] 입금이 완료되었습니다!" << endl;
-					break;
-				}
-				case 2: { // 출금하기
-					string password; int withdraw;
-					cout << "출금액을 입력해주세요. >> "; cin >> withdraw;
-					cout << "비밀번호를 입력해주세요. >> "; cin >> password;
-					if (password == it->second[2]) { // 비밀번호가 맞는지 확인
-						if (withdraw <= stoi(it->second[1])) {
-							it->second[1] = to_string(stoi(it->second[1]) - withdraw);  // 출금하기
-							filewrite(PATH, accounts); // 파일에 저장
-							cout << "[성공] 출금이 완료되었습니다." << endl;
+					case 1: { // 입금하기
+						int deposit;
+						cout << "입금액을 입력해주세요. >> "; cin >> deposit;
+						balance = to_string(stoi(balance) + deposit); // map의 value는 string으로 저장되어 있어서 변환 필요!
+						if (fileWrite(accounts)) {
+							cout << "[성공] 입금이 완료되었습니다!" << endl;
 						}
-						else cout << "[실패] 금액이 부족합니다." << endl;
+						else cout << "[실패] 문제가 발생하였습니다." << endl;
+						break;
 					}
-					else cout << "[실패] 비밀번호가 일치하지 않습니다." << endl;
-					break;
-				}
-				case 3: { // 송금하기
-					string destAccount, password; int amount;
-					cout << "송금을 받을 상대방의 계좌번호를 입력해주세요. >> "; cin >> destAccount;
-					it = accounts.find(destAccount);
-					if (it != accounts.end()) { // 발견시
-						cout << "송금액을 입력해주세요. >> "; cin >> amount;
-						if (stoi(it->second[1]) <= amount) {
-							cout << "비밀번호를 입력해주세요. >> "; cin >> password;
-							if (it->second[2] == password) {
-								it->second[2] = to_string(stoi(it->second[2]) + amount);
-								accounts[currentUser][2] = to_string(stoi(accounts[currentUser][2]) - amount);
-								cout << "[성공] 송금이 완료되었습니다." << endl;
+					case 2: { // 출금하기
+						string checkPassword; int withdraw;
+						cout << "출금액을 입력해주세요. >> "; cin >> withdraw;
+						cout << "비밀번호를 입력해주세요. >> "; cin >> checkPassword;
+						if (checkPassword == password) { // 비밀번호가 맞는지 확인
+							if (withdraw <= stoi(balance)) { // 출금액이 잔액보다 작거나 같은 경우
+								balance = to_string(stoi(balance) - withdraw);  // 출금하기
+								if (fileWrite(accounts)) {
+									cout << "[성공] 출금이 완료되었습니다." << endl;
+								}
+								else cout << "[실패] 문제가 발생하였습니다." << endl;
 							}
-							else cout << "[실패] 비밀번호가 일치하지 않습니다." << endl;
+							else cout << "[실패] 금액이 부족합니다." << endl;
 						}
-						else cout << "[실패] 금액이 부족합니다." << endl;
+						else cout << "[실패] 비밀번호가 일치하지 않습니다." << endl;
+						break;
 					}
-					else cout << "[실패] 해당 계좌번호가 존재하지 않습니다." << endl;
-					break;
-				}
-				case 4: { // 조회하기
-					string password;
-					cout << "비밀번호를 입력해주세요. >> "; cin >> password;
-					if (accounts[currentUser][2] == password) cout << accounts[currentUser][1];
-					else cout << "[실패] 비밀번호가 일치하지 않습니다." << endl;
-					break;
-				}
-				case 5: { // 종료하기
-					cout << "[종료] " << accounts[currentUser][0] << "님의 세션을 종료합니다." << endl;
-					break;
-				}
-				default: {
-					cout << "잘못된 입력입니다. 다시 시도하세요." << endl;
+					case 3: { // 송금하기
+						string destAccount, checkPassword; int amount;
+						map<string, vector<string>>::iterator itDest;
+
+						cout << "송금을 받을 상대방의 계좌번호를 입력해주세요. >> "; cin >> destAccount;
+						itDest = accounts.find(destAccount);
+						if (itDest != accounts.end()) { // 상대방 계좌가 있는 경우
+							string &destBalance = itDest->second[1];
+							cout << "송금액을 입력해주세요. >> "; cin >> amount;
+							if (amount <= stoi(balance)) { // 송금액이 원금액보다 작은 경우
+								cout << "비밀번호를 입력해주세요. >> "; cin >> checkPassword;
+								if (password == checkPassword) {
+									destBalance = to_string(stoi(destBalance) + amount);
+									balance = to_string(stoi(balance) - amount);
+									if (fileWrite(accounts)) {
+										cout << "[성공] 송금이 완료되었습니다." << endl;
+									}
+									else cout << "[실패] 문제가 발생하였습니다." << endl;
+								}
+								else cout << "[실패] 비밀번호가 일치하지 않습니다." << endl;
+							}
+							else cout << "[실패] 금액이 부족합니다." << endl;
+						}
+						else cout << "[실패] 해당 계좌번호가 존재하지 않습니다." << endl;
+						break;
+					}
+					case 4: { // 조회하기
+						string checkPassword;
+						cout << "비밀번호를 입력해주세요. >> "; cin >> checkPassword;
+						if (checkPassword == password) {
+							cout << "[" << owner << "님의 잔액] " << balance << endl;
+						}
+						else cout << "[실패] 비밀번호가 일치하지 않습니다." << endl;
+						break;
+					}
+					case 5: { // 종료하기
+						cout << "[종료] 세션을 종료합니다." << endl;
+						break;
+					}
+					default: {
+						cout << "잘못된 입력입니다. 다시 시도하세요." << endl;
+					}
 				}
 			}
 		}
